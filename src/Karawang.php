9875ca36c22791004389c9bdb7f5181a963b564a
ahmadnorin <ahmadnorin@gmailcom>
2017-10-06 13:57:28 +0700

    update fix

--- src/Karawang.php
+++ src/Karawang.php
@@ -4,7 +4,7 @@
  * @Author: ahmadnorin
  * @Date:   2017-10-06 13:28:45
  * @Last Modified by:   ahmadnorin
- * @Last Modified time: 2017-10-06 13:38:29
+ * @Last Modified time: 2017-10-06 13:57:16
  */
 namespace Ahmadlab\Karawang;
 
@@ -13,5 +13,10 @@ class Karawang
 	public static function Proses()
 	{
 		return 'Package loaded';
+	}
+
+	public static function ahmad()
+	{
+		return 'ahmad juma';
 	}
 }
